96d0a575861fb5396b4ff5b0a4f7decc47966e7a
Nandkishor Rathore <71880979+NandkishorNrr@users.noreply.github.com>
2023-09-18 17:20:27 +0530

    Time: 0 ms (100%), Space: 40.5 MB (56.18%) - LeetHub

--- 0742-to-lower-case/0742-to-lower-case.java
+++ 0742-to-lower-case/0742-to-lower-case.java
@@ -1,11 +1,5 @@
 class Solution {
     public String toLowerCase(String s) {
-        String a = "";
-        for(char ch: s.toCharArray()){
-            if('A' <= ch && ch <= 'Z')
-                ch = (char)(ch + ('a' - 'A'));
-            a += ch; 
-        }
-        return a;
+        return s.toLowerCase();
     }
 }
